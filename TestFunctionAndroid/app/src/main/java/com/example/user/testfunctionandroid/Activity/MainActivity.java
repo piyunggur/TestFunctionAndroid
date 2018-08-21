6fd263915c9ce054e7e181f71b5e790951c8285e
Piyunggur <31981784+piyunggur@users.noreply.github.com>
2018-08-21 15:38:01 +0700

    make project

---
+++ TestFunctionAndroid/app/src/main/java/com/example/user/testfunctionandroid/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.testfunctionandroid.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.testfunctionandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
